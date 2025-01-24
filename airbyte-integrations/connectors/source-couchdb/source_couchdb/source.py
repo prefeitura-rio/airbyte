681464075af6e8a068f45a796711def1862b318d
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2025-01-24 12:56:15 -0300

    feat: add documents stream

--- airbyte-integrations/connectors/source-couchdb/source_couchdb/source.py
+++ airbyte-integrations/connectors/source-couchdb/source_couchdb/source.py
@@ -1,32 +1,15 @@
 #
 # Copyright (c) 2025 Airbyte, Inc., all rights reserved.
 #
-
-
-from abc import ABC
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+from typing import Any, List, Mapping, Tuple
 
 import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthenticator
+from airbyte_cdk.sources.streams.http.requests_native_auth import BasicHttpAuthenticator
 from requests.exceptions import ConnectionError, Timeout, HTTPError
 
-from .streams import Customers, Employees
-
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
+from .streams import Documents
 
 class SourceCouchdb(AbstractSource):
     def check_connection(self, logger, config) -> Tuple[bool, any]:
@@ -85,10 +68,18 @@ class SourceCouchdb(AbstractSource):
         
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
-        TODO: Replace the streams below with your own streams.
-
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        try:
+            host = config["host"]
+            port = config["port"]
+            username = config["username"]
+            password = config["password"]
+            database = config["database"]
+        except KeyError as e:
+            return False, f"KeyError: {str(e)} is required."
+
+        authenticator = BasicHttpAuthenticator(username=username, password=password)
+        url_base = f"http://{host}:{port}/{database}"
+
+        return [Documents(url_base=url_base, authenticator=authenticator)]
