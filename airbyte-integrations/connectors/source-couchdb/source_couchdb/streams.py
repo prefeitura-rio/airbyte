761a12f55037f334863b87cd7a3f820c41fe5926
d116626 <d116626@gmail.com>
2025-01-27 18:54:33 -0300

    chore: add incremental stream using _changes

--- airbyte-integrations/connectors/source-couchdb/source_couchdb/streams.py
+++ airbyte-integrations/connectors/source-couchdb/source_couchdb/streams.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2024 Airbyte, Inc., all rights reserved.
 
 from abc import ABC
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Any, Iterable, Mapping, MutableMapping, Optional
 
 import requests
@@ -78,16 +79,7 @@ class Documents(CouchdbStream):
 
 
 # Basic incremental stream
-class IncrementalCouchdbStream(HttpStream, ABC):
-    def __init__(self, url_base: str, page_size: int, *args, **kwargs):
-        self.__url_base = url_base
-        self.__page_size = page_size
-        return super().__init__(*args, **kwargs)
-
-    @property
-    def url_base(self) -> str:
-        return self.__url_base
-
+class IncrementalCouchdbStream(CouchdbStream, ABC):
     state_checkpoint_interval = 1000
 
     @property
@@ -100,8 +92,15 @@ class IncrementalCouchdbStream(HttpStream, ABC):
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
-        _changes api does not use pagination.
+        _changes api  uses the last_seq to control the pagination
+        if pending is not 0 there are more data, return the next_page_token.
         """
+        response_json = response.json()
+        pending = response_json.get("pending", 0)
+        last_seq = response_json.get("last_seq")
+        self.logger.info(f"Pending: {pending}")
+        if pending > 0 and last_seq:
+            return {"since": last_seq}
         return None
 
     def get_updated_state(
@@ -113,7 +112,7 @@ class IncrementalCouchdbStream(HttpStream, ABC):
         Updates the stream state based on the most recent record.
         """
         current_cursor_value = current_stream_state.get(self.cursor_field, None)
-        latest_cursor_value = latest_record.get("seq")
+        latest_cursor_value = latest_record.get("last_seq")
 
         if current_cursor_value is None:
             return {self.cursor_field: latest_cursor_value}
@@ -149,7 +148,7 @@ class DocumentsIncremental(IncrementalCouchdbStream):
     """
 
     # Define o campo que será usado como cursor para a sincronização incremental
-    cursor_field = "seq"
+    cursor_field = "last_seq"
 
     # Define a chave primária para o stream
     primary_key = "id"
@@ -162,33 +161,55 @@ class DocumentsIncremental(IncrementalCouchdbStream):
     ) -> str:
         return "_changes"
 
-    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        """
-        :return an iterable containing each record in the response
-        """
-        response_json = response.json()
-        if "results" not in response_json:
-            raise KeyError("Response does not contain 'results' field.")
-        last_seq = response_json["last_seq"]
-        for row in response_json["results"]:
-            yield self._fetch_document(row=row, last_seq=last_seq)
+    def process_document(self, result, rows, last_seq):
+        if "docs" not in result or not result["docs"]:
+            self.logger.warning(f"No 'docs' field in result: {result}")
+            return None
 
-    def _fetch_document(self, row: Mapping, last_seq: str = None) -> Mapping:
-        doc_id = row["id"]
-        url = f"{self.url_base}{doc_id}"
-        self.logger.info(f"Fetching document: {url}")
-        response = self._session.get(url)
-        response.raise_for_status()
-        doc = response.json()
+        # Extract the document from the nested structure
+        doc = result["docs"][0].get("ok", {})
+        if "_id" not in doc:
+            self.logger.warning(f"Document missing '_id' field: {doc}")
+            return None
+
+        # Match the document with the corresponding row to get the sequence number
+        seq = next((row["seq"] for row in rows if row["id"] == doc["_id"]), None)
+        if seq is None:
+            self.logger.warning(f"No matching row found for document: {doc}")
+            return None
 
         return {
             "id": doc.get("_id"),
             "key": doc.get("_id"),
             "value": {"rev": doc.get("_rev")},
             "doc": doc,
-            "seq": row.get("seq"),
+            "seq": seq,
             "last_seq": last_seq,
         }
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        response_json = response.json()
+        if "results" not in response_json:
+            raise KeyError("Response does not contain 'results' field.")
+        last_seq = response_json["last_seq"]
+        rows = response_json["results"]
+
+        # Prepare bulk request
+        bulk_docs = {"docs": [{"id": row["id"]} for row in rows]}
+        url = f"{self.url_base}_bulk_get"
+        bulk_response = self._session.post(url, json=bulk_docs)
+
+        bulk_response.raise_for_status()
+        bulk_docs_response = bulk_response.json()
+
+        # Use ThreadPoolExecutor to process documents in parallel
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(self.process_document, result, rows, last_seq) for result in bulk_docs_response["results"]]
+
+            for future in as_completed(futures):
+                result = future.result()
+                if result is not None:
+                    yield result
 
     def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
         """
